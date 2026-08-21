4b250346b83d2d5fd4fbe870fe184a4a88f65a0d
juanmacosta-it <juanmacosta.it@gmail.com>
2026-08-21 11:24:56 -0300

    chore: bootstrap spring boot project with security, jpa and web starters

---
+++ src/test/java/com/securevault/secure_vault/SecureVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securevault.secure_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
